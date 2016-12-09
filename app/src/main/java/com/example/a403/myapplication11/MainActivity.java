65a0b0ebc3f13f71dc12bb40e268a5b528a1606a
myname <myname@test.com>
2016-12-09 11:47:13 +0900

    사진 추가 라디오버튼 기능추가

--- app/src/main/java/com/example/a403/myapplication11/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication11/MainActivity.java
@@ -8,8 +8,21 @@ import android.widget.Switch;
 import android.widget.Chronometer;
 import android.widget.LinearLayout;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private double totalAmount=0;
+    final int adult=15000;
+    final int teenager=12000;
+    final int child = 8000;
+    EditText et1,et2,et3;
+    RadioGroup rg1;
+    ImageView iv1;
+    TextView tv1,tv2,tv3;
+
     Switch swi_start;
     Chronometer crono;
     LinearLayout layand;
@@ -20,7 +33,11 @@ public class MainActivity extends AppCompatActivity {
         swi_start = (Switch)findViewById(R.id.swi_start);
         crono = (Chronometer) findViewById(R.id.chronometer2);
         layand = (LinearLayout)findViewById(R.id.landSystem);
-
+        et1 = (EditText)findViewById(R.id.editText1);
+        et2 = (EditText)findViewById(R.id.editText2);
+        et3 = (EditText)findViewById(R.id.editText3);
+        rg1 = (RadioGroup)findViewById(R.id.radioGroup);
+        iv1 = (ImageView)findViewById(R.id.imageView);
         swi_start.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -30,6 +47,21 @@ public class MainActivity extends AppCompatActivity {
                     layand.setVisibility(View.VISIBLE);
                 } else {
                     layand.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId==R.id.radioButton){
+                    iv1.setImageResource(R.drawable.land1);
+                }
+                else if(checkedId==R.id.radioButton2){
+                    iv1.setImageResource(R.drawable.land2);
+                }
+                else{
+                    iv1.setImageResource(R.drawable.land3);
                 }
             }
         });
